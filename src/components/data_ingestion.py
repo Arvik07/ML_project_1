2bc502e7c6efd88a6e5e3fd711bb51a83a18d8ef
Arvik07 <krishn156189@gmail.com>
2025-08-18 17:59:47 +0530

    model_trainer commi

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -5,10 +5,12 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 import logging
 from src.components import data_transformation
+from src.components import model_trainer
 from src.exception import CustomException
 from src.components.data_transformation import DataTransformationConfig
 from src.components.data_transformation import DataTransformation
-
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
 
 @dataclass
 class DataIngestionConfig:
@@ -52,4 +54,7 @@ if __name__ == '__main__':
     train_path, test_path = obj.initiate_data_ingestion()
 
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_path, test_path)
+    train_arr,test_arr,_  = data_transformation.initiate_data_transformation(train_path, test_path)
+
+    model_trainer = ModelTrainer()
+    print(model_trainer.initiate_model_trainer(train_arr, test_arr))
---
+++ src/components/model_trainer.py
@@ -0,0 +1,77 @@
+import os
+from pyclbr import Class
+import sys
+from dataclasses import dataclass
+from turtle import mode
+from catboost import CatBoostRegressor
+from sklearn.ensemble import(
+    AdaBoostRegressor,
+    GradientBoostingRegressor,
+    RandomForestRegressor
+)
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.tree import DecisionTreeRegressor
+from xgboost import XGBRegressor
+
+from src.exception import CustomException
+from src.logger import logging
+from src.utils import save_object,evaluate_models
+
+@dataclass
+class ModelTrainerConfig:
+    trained_model_file_path = os.path.join('artifacts', 'model.pkl')
+
+class ModelTrainer:
+    def __init__(self):
+        self.model_trainer_config = ModelTrainerConfig()
+
+    def initiate_model_trainer(self, train_array, test_array):
+        try:
+            logging.info("Splitting training and testing input features and target features") 
+            X_train, y_train, X_test, y_test = (
+                train_array[:, :-1],
+                train_array[:, -1],
+                test_array[:, :-1],
+                test_array[:, -1]
+            ) 
+            models = {
+                "Linear Regression": LinearRegression(),
+                "Decision Tree": DecisionTreeRegressor(),
+                "Random Forest": RandomForestRegressor(),
+                "Gradient Boosting": GradientBoostingRegressor(),
+                "AdaBoost Classifier": AdaBoostRegressor(),
+                "KNN": KNeighborsRegressor(),
+                "XGBClassifier": XGBRegressor(),
+                "CatBoosting Classifier": CatBoostRegressor(),
+                "K-Neighbors Classifier": KNeighborsRegressor()
+            }
+
+            model_report:dict = evaluate_models(X_train, y_train, X_test, y_test, models)
+
+            ## To get best model score from dict
+            best_model_score = max(sorted(model_report.values()))
+
+            ## To get best model name from dict
+            best_model_name = list(model_report.keys())[list(model_report.values()).index(best_model_score)]
+
+            best_model = models[best_model_name]
+
+            if best_model_score < 0.6:
+                raise CustomException("No best model found")
+            logging.info(f"Best model found: {best_model_name} with score: {best_model_score}")
+
+            save_object(
+                file_path=self.model_trainer_config.trained_model_file_path,
+                obj=best_model
+            )
+
+            predicted = best_model.predict(X_test)
+            r2_score_value = r2_score(y_test, predicted)
+            return r2_score_value
+        
+
+        except Exception as e:
+            logging.error("Error occurred while training model")
+            raise CustomException(e, sys)
